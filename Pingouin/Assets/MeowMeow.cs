3af66aed7e6af894fee310b94e2a91e908b7f01a
alexkabil <alexandre.kabil@gmail.com>
2014-12-21 11:54:00 +0100

    Changes Now
    Destroy old to put new

---
+++ Pingouin/Assets/MeowMeow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeowMeow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void SetPlat(){
+	GameObject go;
+		go = GameObject.Instantiate(Resources.Load("Blood")) as GameObject;
+		go.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition)+ new Vector3(0,0,9);
+		go.transform.parent = transform;
+		StartCoroutine(go.GetComponent<Splash>().LifeTime());
+		GameObject.Find("Meowww").audio.Play();
+	}
+	
+	void OnMouseDown() {
+	SetPlat();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Pingouin/Assets/Splash.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public IEnumerator LifeTime(){
+		yield return new WaitForSeconds(3.0f);
+		GameObject.Destroy(this.gameObject);
+		yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
